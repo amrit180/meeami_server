b3428dd29f2922693185215a2993274d3a4d3150
AMRITANSHU SINGH <amritanshusingh@AMRITANSHUs-MacBook-Air.local>
2023-09-23 16:01:10 +0530

    mongo removed

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
+
 const morgan = require("morgan");
 const config = require("./config/config");
 
 const router = require("./routes");
 
-mongoose.set("strictQuery", true);
-mongoose.connect(config.mongoose.url, config.mongoose.options);
+// mongoose.set("strictQuery", true);
+// mongoose.connect(config.mongoose.url, config.mongoose.options);
 
-mongoose.connection.on("connected", function () {
-  console.log("Mongoose default connection open");
-});
+// mongoose.connection.on("connected", function () {
+//   console.log("Mongoose default connection open");
+// });
 
-// If the connection throws an error
-mongoose.connection.on("error", function (err) {
-  console.log("Mongoose default connection error: " + err);
-});
+// // If the connection throws an error
+// mongoose.connection.on("error", function (err) {
+//   console.log("Mongoose default connection error: " + err);
+// });
 
-// When the connection is disconnected
-mongoose.connection.on("disconnected", function () {
-  console.log("Mongoose default connection disconnected");
-});
+// // When the connection is disconnected
+// mongoose.connection.on("disconnected", function () {
+//   console.log("Mongoose default connection disconnected");
+// });
 
-// If the Node process ends, close the Mongoose connection
-process.on("SIGINT", function () {
-  mongoose.connection.close();
-});
+// // If the Node process ends, close the Mongoose connection
+// process.on("SIGINT", function () {
+//   mongoose.connection.close();
+// });
 
 app.use(morgan("dev"));
 app.use(cors());
